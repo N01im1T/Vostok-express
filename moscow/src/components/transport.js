294404f036fea2a481ad772c3e19f2d5bc704a87
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-07 13:39:13 +1000

    header

---
+++ moscow/src/components/transport.js
@@ -0,0 +1,5 @@
+const transport = () => {
+    
+}
+
+export default transport;
--- moscow/src/main.js
+++ moscow/src/main.js
@@ -1,21 +1,17 @@
-import './assets/styles/main.css';
-import '../public/en/index.html';
-import '../public/ru/index.html';
-
-import { initHeader } from './components/header.js';
-import { initFooter } from './components/footer.js';
-import { initTransport } from './components/transport.js';
-import { initCalculator } from './components/calculator.js';
-import { initSteps } from './components/steps.js';
-import { initButtons } from './components/buttons.js';
-import { initModals } from './components/modals.js';
+import header from './components/header.js';
+import footer from './components/footer.js';
+import transport from './components/transport.js';
+import calculator from './components/calculator.js';
+import steps from './components/steps.js';
+import buttons from './components/buttons.js';
+import modals from './components/modals.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    initHeader();
-    initFooter();
-    initTransport();
-    initCalculator();
-    initSteps();
-    initModals();
-    initButtons();
+    header();
+    footer();
+    transport();
+    calculator();
+    steps();
+    modals();
+    buttons();
 });
