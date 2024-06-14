92cc02b3d98f1762abe984aa8dad9e47247d2fd3
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-14 10:59:03 +1000

    feat: about-company section

--- moscow/src/main.js
+++ moscow/src/main.js
@@ -3,16 +3,18 @@ import footer from './components/footer.js';
 import transport from './components/transport.js';
 import calculator from './components/calculator.js';
 import steps from './components/steps.js';
+import aboutCompany from './components/about-company.js';
 import buttons from './components/buttons.js';
 import inputs from './components/inputs.js';
 import modals from './components/modals.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     header();
     footer();
     transport();
     calculator();
     steps();
+    aboutCompany();
     buttons();
     inputs();
     modals();
