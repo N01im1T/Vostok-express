149ba9bce9e100affa78ac586c778bb651334307
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-17 17:18:27 +1000

    aria-labelledby

--- moscow/src/main.js
+++ moscow/src/main.js
@@ -3,7 +3,7 @@ import footer from './components/footer.js';
 import transport from './components/transport.js';
 import calculator from './components/calculator.js';
 import steps from './components/steps.js';
-import aboutCompany from './components/about-company.js';
+// import aboutCompany from './components/about-company.js';
 import buttons from './components/buttons.js';
 import inputs from './components/inputs.js';
 import modals from './components/modals.js';
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     transport();
     calculator();
     steps();
-    aboutCompany();
+    // aboutCompany();
     buttons();
     inputs();
     modals();
