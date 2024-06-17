af6c04966b6a342014668914a2b6436c565c45e1
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-17 17:18:27 +1000

    aria-labelledby

--- moscow/src/components/buttons.js
+++ moscow/src/components/buttons.js
@@ -1,16 +1,52 @@
 const buttons = () => {
-    const toggleLanguage = () => {
-        var ru = document.getElementById('ru');
-        var en = document.getElementById('en');
+    // const toggleLanguage = () => {
+    //     var ru = document.getElementById('ru');
+    //     var en = document.getElementById('en');
         
-        if (ru.classList.contains('active')) {
-            ru.classList.remove('active');
-            en.classList.add('active');
-        } else {
-            en.classList.remove('active');
-            ru.classList.add('active');
+    //     if (ru.classList.contains('active')) {
+    //         ru.classList.remove('active');
+    //         en.classList.add('active');
+    //     } else {
+    //         en.classList.remove('active');
+    //         ru.classList.add('active');
+    //     }
+    // };
+
+    const toggleBtn = document.getElementById('toggle-btn');
+    const ruBtn = document.getElementById('ru');
+    const enBtn = document.getElementById('en');
+
+    // Check site language
+    document.addEventListener('DOMContentLoaded', function() {
+        const enSelected = enBtn.getAttribute('aria-selected') === 'true';
+        if (enSelected) {
+            document.querySelector('.indicator').style.transform = 'translateX(0)';
         }
-    };
+    });
+
+    function switchLanguage(language) {
+        if (language === 'ru') {
+            window.location.href = '/moscow/public/ru/index.html';
+        } else if (language === 'en') {
+            window.location.href = '/moscow/public/en/index.html';
+        }
+    }
+
+    toggleBtn.addEventListener('click', function() {
+        const ruSelected = ruBtn.getAttribute('aria-selected') === 'true';
+
+        if (ruSelected) {
+            ruBtn.setAttribute('aria-selected', 'false');
+            enBtn.setAttribute('aria-selected', 'true');
+            document.querySelector('.indicator').style.transform = 'translateX(0)';
+            switchLanguage('en');
+        } else {
+            ruBtn.setAttribute('aria-selected', 'true');
+            enBtn.setAttribute('aria-selected', 'false');
+            document.querySelector('.indicator').style.transform = 'translateX(100%)';
+            switchLanguage('ru');
+        }
+    });
 }
 
 
