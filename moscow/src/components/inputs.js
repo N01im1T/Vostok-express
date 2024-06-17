1e5b51106d16d995ed4bc7203dc386a0d0ca9935
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-17 14:22:45 +1000

    footer / modal windows / buttons refactoring

--- moscow/src/components/inputs.js
+++ moscow/src/components/inputs.js
@@ -1,24 +1,28 @@
 const inputs = () => {
     document.querySelectorAll('.styled-input').forEach(input => {
-        input.addEventListener('input', () => {
-            const label = input.nextElementSibling;
-            if (input.value.trim() === '') {
-                label.textContent = label.getAttribute('data-original-text');
-                return
-            }
+        if (input.id !== 'message') {    
+            input.addEventListener('input', () => {
+                const label = input.nextElementSibling;
+                if (input.value.trim() === '') {
+                    label.textContent = label.getAttribute('data-original-text');
+                    return
+                }
 
-            if (input.validity.valid) {
-                input.classList.remove('invalid');
-                label.textContent = label.getAttribute('data-original-text');
-            } else {
-                input.classList.add('invalid');
-                label.textContent = label.getAttribute('data-error-message');
-            }
-        });
+                if (input.validity.valid) {
+                    input.classList.remove('invalid');
+                    label.textContent = label.getAttribute('data-original-text');
+                } else {
+                    input.classList.add('invalid');
+                    label.textContent = label.getAttribute('data-error-message');
+                }
+            });
+        }   
     });
     
-    document.getElementById('telephone').addEventListener('input', function (e) {
-        this.value = this.value.replace(/[a-zA-Zа-яА-Я]/g, '');
+    document.querySelectorAll('input[type="tel"]').forEach(function (input) {
+        input.addEventListener('input', function (e) {
+            this.value = this.value.replace(/[a-zA-Zа-яА-Я]/g, '');
+        });
     });
 }
 
--- moscow/src/components/modals.js
+++ moscow/src/components/modals.js
@@ -1,5 +1,53 @@
 const modals = () => {
-    
+    var modalCallMeBack = document.getElementById("modal-call-me-back");
+    var modalBecomeOurPartner = document.getElementById("modal-become-our-partner");
+    var modalGetTransferPrice = document.getElementById("modal-get-transfer-price");
+
+    var btnCallMeBack = document.getElementById("btn-call-me-back");
+    var btnBecomeOurPartner = document.getElementById("btn-become-our-partner");
+    var btnGetTransferPrice = document.getElementById("btn-get-transfer-price");
+
+    const modals = [modalCallMeBack, modalBecomeOurPartner, modalGetTransferPrice];
+
+    btnCallMeBack.addEventListener('click', function() {
+        toggleDiv(modalCallMeBack);
+    });
+
+    btnBecomeOurPartner.addEventListener('click', function() {
+        toggleDiv(modalBecomeOurPartner);
+    });
+
+    btnGetTransferPrice.addEventListener('click', function() {
+        toggleDiv(modalGetTransferPrice);
+    });
+
+    modals.forEach(modal => {
+        modal.querySelector('.close-icon').addEventListener('click', function() {
+            closeDiv(modal);
+        });
+        
+        window.addEventListener('click', function(event) {
+            modals.forEach(modal => {        
+                if (event.target == modal) {
+                    closeDiv(modal);
+                }
+            });
+        });
+    });
+
+    function toggleDiv(div) {
+        div.classList.remove('fade-out');
+        div.classList.add('fade-in');
+        div.style.display = "block";
+    }
+
+    function closeDiv(div) {
+        div.classList.remove('fade-in');
+        div.classList.add('fade-out');
+        setTimeout(() => {
+            div.style.display = "none";
+        }, 500);
+    }
 }
 
 export default modals;
