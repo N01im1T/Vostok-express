98bc5a7e15a7d8884c0a473ea22f64bc14732b1d
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-18 13:29:31 +1000

    refactoring

--- moscow/src/components/buttons.js
+++ moscow/src/components/buttons.js
@@ -1,17 +1,4 @@
 const buttons = () => {
-    // const toggleLanguage = () => {
-    //     var ru = document.getElementById('ru');
-    //     var en = document.getElementById('en');
-        
-    //     if (ru.classList.contains('active')) {
-    //         ru.classList.remove('active');
-    //         en.classList.add('active');
-    //     } else {
-    //         en.classList.remove('active');
-    //         ru.classList.add('active');
-    //     }
-    // };
-
     const toggleBtn = document.getElementById('toggle-btn');
     const ruBtn = document.getElementById('ru');
     const enBtn = document.getElementById('en');
