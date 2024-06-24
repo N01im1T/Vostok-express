7c57f15bebd7c6a5bad8e07a10852843513f90a1
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-06-24 18:35:44 +1000

    feat: site adaptive

--- moscow/src/components/about-company.js
+++ moscow/src/components/about-company.js
@@ -1,18 +1,60 @@
-// import Splide from '@splidejs/splide';
-// import '@splidejs/splide/dist/css/splide.min.css';
+const aboutCompany = () => {
+    const slides = document.querySelectorAll('.awards-slider-slide');
+    const slider = document.querySelector('.awards-slider');
+    const slideList = document.querySelector('.awards-slides');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const paginationDots = document.querySelectorAll('.pagination-dot');
+    var currentIndex = 0;
 
-// const aboutCompany = () => {
-//     var splide = new Splide( '.splide', {
-//         type   : 'loop',
-//         perPage: 1,
-//         drag   : 'free',
-//         snap   : true,
-//         lazyLoad: 'nearby',
-//         pagination: true,
-//         arrows : true,
-//     });
+    function updateSliderHeight() {
+        var maxHeight = 0;
 
-//     splide.mount();
-// }
+        slides.forEach(slide => {
+            const height = slide.scrollHeight;
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
+        });
 
-// export default aboutCompany;
+        slider.style.height = maxHeight + 'px';
+    }
+
+    function updateSlidePosition() {
+        slides.forEach((slide, index) => {
+            slide.classList.toggle('active', index === currentIndex);
+        });
+        paginationDots.forEach((dot, index) => {
+            dot.classList.toggle('active', index === currentIndex);
+        });
+        updateSliderHeight();
+    }
+
+    function showNextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateSlidePosition();
+    }
+
+    function showPrevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        updateSlidePosition();
+    }
+
+    prevButton.addEventListener('click', showPrevSlide);
+    nextButton.addEventListener('click', showNextSlide);
+
+    paginationDots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            currentIndex = index;
+            updateSlidePosition();
+        });
+    });
+
+    updateSlidePosition();
+
+    window.addEventListener('resize', () => {
+        updateSliderHeight();
+    });
+}
+
+export default aboutCompany;
