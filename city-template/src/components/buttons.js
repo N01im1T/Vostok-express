fb43839f09aeb54f82653ef9f9f514a96e623b18
N01im1T <lavrinaitis.v@yandex.ru>
2024-07-16 15:42:09 +1000

    fix: check toggleButton and svg element existence

--- city-template/src/components/buttons.js
+++ city-template/src/components/buttons.js
@@ -5,14 +5,9 @@ const buttons = () => {
 
   // Check site language
   document.addEventListener("DOMContentLoaded", function () {
-    if (enBtn) {
-      const enSelected = enBtn.getAttribute("aria-selected") === "true";
-      if (enSelected) {
-        const indicator = document.querySelector(".indicator");
-        if (indicator) {
-          indicator.style.transform = "translateX(0)";
-        }
-      }
+    const enSelected = enBtn.getAttribute("aria-selected") === "true";
+    if (enSelected) {
+      document.querySelector(".indicator").style.transform = "translateX(0)";
     }
   });
 
@@ -24,29 +19,21 @@ const buttons = () => {
     }
   }
 
-  if (toggleBtn && ruBtn && enBtn) {
-    toggleBtn.addEventListener("click", function () {
-      const ruSelected = ruBtn.getAttribute("aria-selected") === "true";
+  toggleBtn.addEventListener("click", function () {
+    const ruSelected = ruBtn.getAttribute("aria-selected") === "true";
 
-      if (ruSelected) {
-        ruBtn.setAttribute("aria-selected", "false");
-        enBtn.setAttribute("aria-selected", "true");
-        const indicator = document.querySelector(".indicator");
-        if (indicator) {
-          indicator.style.transform = "translateX(0)";
-        }
-        switchLanguage("en");
-      } else {
-        ruBtn.setAttribute("aria-selected", "true");
-        enBtn.setAttribute("aria-selected", "false");
-        const indicator = document.querySelector(".indicator");
-        if (indicator) {
-          indicator.style.transform = "translateX(100%)";
-        }
-        switchLanguage("ru");
-      }
-    });
-  }
+    if (ruSelected) {
+      ruBtn.setAttribute("aria-selected", "false");
+      enBtn.setAttribute("aria-selected", "true");
+      document.querySelector(".indicator").style.transform = "translateX(0)";
+      switchLanguage("en");
+    } else {
+      ruBtn.setAttribute("aria-selected", "true");
+      enBtn.setAttribute("aria-selected", "false");
+      document.querySelector(".indicator").style.transform = "translateX(100%)";
+      switchLanguage("ru");
+    }
+  });
 };
 
 export default buttons;
